624901b3e12a4c61f122c2c6831f463445f7dcc8
Catalin Niculescu <cn.cata@gmail.com>
2020-01-22 21:18:55 +0200

    input data manipulation

--- 01_kernel_py.py
+++ 01_kernel_py.py
@@ -27,12 +27,28 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 # print("csv row length: " + str(readCSV.line_num)) 
 # print("sizeof(row trasnformed into numpy obj): " + str(np.array(rows).nbytes) + " in bytes")
 
-
 import os
+import csv
+import numpy as np
 import pandas as pd
+from zlib import crc32
+import matplotlib
+matplotlib.use('agg') # explicitly asked for a non-GUI backend
+import matplotlib.pyplot as plt
 
-def load_data():
-    return pd.read_csv(os.path.abspath(os.path.join('input', 'sales_train.csv')))
+sales_train = pd.read_csv(os.path.abspath(os.path.join('input', 'sales_train.csv')))
+sales_train.drop("date", axis=1, inplace=True)
 
-sales_train = load_data()
-print(sales_train)
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+sales_train_with_id = sales_train.reset_index()   # adds an `index` column
+train_set, test_set = split_train_test_by_id(sales_train_with_id, 0.2, "index")
+
+train_set.plot(kind="scatter", x="date_block_num", y="item_cnt_day")
+plt.show()
