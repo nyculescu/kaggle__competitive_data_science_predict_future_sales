c1f5730d0c3dd8535fad1bcd6f81d1a5cbe1d250
Catalin Niculescu <cn.cata@gmail.com>
2020-01-23 15:19:17 +0200

    data analysis

--- 01_kernel_py.py
+++ 01_kernel_py.py
@@ -32,12 +32,18 @@ import csv
 import numpy as np
 import pandas as pd
 from zlib import crc32
-import matplotlib
-matplotlib.use('agg') # explicitly asked for a non-GUI backend
+# import matplotlib
+# matplotlib.use('agg') # explicitly asked for a non-GUI backend
 import matplotlib.pyplot as plt
 
 sales_train = pd.read_csv(os.path.abspath(os.path.join('input', 'sales_train.csv')))
 sales_train.drop("date", axis=1, inplace=True)
+
+def describe_entire_nrs(datafr):
+    desc = datafr.describe() # include='all' will not work
+    desc.loc['count'] = desc.loc['count'].astype(int).astype(str)
+    desc.iloc[1:] = desc.iloc[1:].applymap('{:.2f}'.format)
+    return desc
 
 def test_set_check(identifier, test_ratio):
     return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
@@ -47,8 +53,23 @@ def split_train_test_by_id(data, test_ratio, id_column):
     in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
     return data.loc[~in_test_set], data.loc[in_test_set]
 
+sales_train = sales_train[~(sales_train['item_cnt_day'] < 0)]
+sales_train = sales_train[~(sales_train['item_price'] < 0)]
+
 sales_train_with_id = sales_train.reset_index()   # adds an `index` column
 train_set, test_set = split_train_test_by_id(sales_train_with_id, 0.2, "index")
 
-train_set.plot(kind="scatter", x="date_block_num", y="item_cnt_day")
+# plt.scatter(train_set["date_block_num"], train_set["item_cnt_day"])
+# plt.show()
+
+# corr_matrix = train_set.corr()
+# print(corr_matrix["item_cnt_day"].sort_values(ascending=False))
+# print(corr_matrix["date_block_num"].sort_values(ascending=False))
+# print(corr_matrix["item_price"].sort_values(ascending=False))
+# print(corr_matrix["shop_id"].sort_values(ascending=False))
+# print(corr_matrix["item_id"].sort_values(ascending=False))
+
+from pandas.plotting import scatter_matrix
+attributes = ["date_block_num", "item_cnt_day", "item_price", "shop_id", "item_id"]
+scatter_matrix(train_set[attributes], figsize=(12, 8))
 plt.show()
