92fedb41d5ddb65ad43c75576fc88149a54ffc47
Catalin Niculescu <cn.cata@gmail.com>
2020-02-05 20:11:36 +0200

    <Fine-tune methods> - updated <Evaluate the System on the Test Set> - created

--- 01_kernel_py.py
+++ 01_kernel_py.py
@@ -13,10 +13,12 @@ import numpy as np
 import os
 import csv
 import pandas as pd
-
+from pandas.plotting import scatter_matrix
+import datetime
+from sklearn.externals import joblib
+from sklearn.metrics import mean_squared_error
 
 # To plot pretty figures
-# %matplotlib inline
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 mpl.rc('axes', labelsize=14)
@@ -63,23 +65,59 @@ def split_train_test_by_id(data, test_ratio, id_column):
     in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
     return data.loc[~in_test_set], data.loc[in_test_set]
 
-sales_train = pd.read_csv(os.path.abspath(os.path.join('input', 'sales_train.csv')))
+colab_path = "drive/My Drive/Colab Notebooks/kaggle_competitive_data_science_predict_future_sales"
 
 # -------------------------------------------------------------------
 
-sales_train_gb_mth = sales_train.drop(['item_id', 'item_price'], axis = 1).groupby(['shop_id', 'date_block_num']).sum().astype('int')
-sales_train_gb_mth.rename(columns={"item_cnt_day": "item_cnt_month"}, inplace=True)
-sales_train_ = sales_train.copy()
-sales_train_['item_cnt_month'] = int(0)
-sales_train_.drop(["date"], inplace=True, axis=1)
+sales_train_ = pd.read_csv(os.path.abspath(os.path.join('input', 'sales_train_reshaped.csv')))
 
-# %%time
-for row in sales_train_.iterrows():
-  idx_s = int(sales_train_.loc[sales_train_.index[row[0]], "shop_id"])
-  idx_m = int(sales_train_.loc[sales_train_.index[row[0]], "date_block_num"])
+sales_train_.drop('Unnamed: 0', axis=1, inplace=True)
 
-#   item_cnt_month = sales_train_gb_mth["item_cnt_month"][idx_s][idx_m] # this returns with the value 2017
-  item_cnt_month = sales_train_gb_mth["item_cnt_month"]
-  sales_train_.loc[sales_train_.index[row[0]], "item_cnt_month"] = int(item_cnt_month[idx_s][idx_m])
+from sklearn.model_selection import train_test_split
+train_set, test_set = train_test_split(sales_train_, train_size=0.8, shuffle=False) 
 
-sales_train_.to_csv(r"input/sales_train_reshaped.csv")
+sales_train_ts = train_set.copy()
+
+sales_train_ts_lb = sales_train_ts['item_cnt_month'].copy()
+sales_train_ts.drop('item_cnt_month', axis=1, inplace=True)
+
+from sklearn.preprocessing import OneHotEncoder
+item_cat_1hot = OneHotEncoder().fit_transform(sales_train_ts[["item_id"]])
+shop_cat_1hot = OneHotEncoder().fit_transform(sales_train_ts[["shop_id"]])
+attr_to_be_dropped = {
+    "item_id",
+    "shop_id"
+    }
+sales_train_num = sales_train_ts.drop(attr_to_be_dropped, axis=1)
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+num_pipeline = Pipeline([ # a small pipeline for the numerical attributes
+        ('imputer', SimpleImputer(strategy="median")),
+        # ('attribs_adder', CombinedAttributesAdder()),
+        ('std_scaler', StandardScaler()),
+    ])
+sales_train_num_tr = num_pipeline.fit_transform(sales_train_num)
+
+from sklearn.compose import ColumnTransformer
+num_attribs = list(sales_train_num)
+full_pipeline = ColumnTransformer([
+        ("num", num_pipeline, num_attribs),
+        ("itm", OneHotEncoder(), ["item_id"]),
+        ("shp", OneHotEncoder(), ["shop_id"]),
+    ])
+sales_train_ts_prep = full_pipeline.fit_transform(sales_train_ts)
+
+from sklearn.ensemble import RandomForestRegressor
+print(datetime.datetime.now())
+forest_reg = RandomForestRegressor(n_estimators=100, random_state=42)
+forest_reg.fit(sales_train_ts_prep, sales_train_ts_lb)
+print(datetime.datetime.now())
+
+sales_train_predictions = forest_reg.predict(sales_train_ts_prep)
+forest_mse = mean_squared_error(sales_train_ts_lb, sales_train_predictions)
+forest_rmse = np.sqrt(forest_mse)
+forest_rmse
+
+joblib.dump(forest_reg, os.path.abspath(os.path.join('models', 'forest_reg.pkl')))
